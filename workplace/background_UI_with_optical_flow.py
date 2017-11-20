ec956f4ff49e70c0e854b29f69a43f77e0fdc63c
yurimmm <ikisee@naver.com>
2017-11-20 18:44:10 +0900

    프레임 밖으로 나가는 현상 해결
    Signed-off-by: yurimmm <ikisee@naver.com>

--- workplace/background_UI_with_optical_flow.py
+++ workplace/background_UI_with_optical_flow.py
@@ -58,8 +58,9 @@ if __name__ == '__main__':
     old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
 
     # Set tracking points
-    p0 = np.array([[[200.0,200.0]]])
+    p0 = np.array([[[200.0,200.0]], [[180.0,180.0]]])
     p0 = np.float32(p0)
+    #p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
 
     # Create a mask image for drawing purposes
     mask = np.zeros_like(old_frame)
@@ -67,6 +68,7 @@ if __name__ == '__main__':
     while(1):
 
         ret,realframe = cap.read()
+        realframe = cv2.flip(realframe,1)  #좌우반전
 
         #trackingframe = realframe
         frame_gray = cv2.cvtColor(realframe, cv2.COLOR_BGR2GRAY)
@@ -77,33 +79,39 @@ if __name__ == '__main__':
         good_new = p1[st==1]
         good_old = p0[st==1]
 
-        # draw the tracks
-        for i,(new,old) in enumerate(zip(good_new,good_old)):
-            a,b = new.ravel()
-            c,d = old.ravel()
-            checkHandPosition(a,b)
-            mask = cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2)
-            realframe = cv2.circle(realframe,(a,b),5,color[i].tolist(),-1)
+        if(st.all() == 1):
+
+            # draw the tracks
+            for i,(new,old) in enumerate(zip(good_new,good_old)):
+                a,b = new.ravel() #현재 프레임의 좌표값
+                c,d = old.ravel() #이전 프레임의 좌표값
+                #checkHandPosition(a,b)
+                mask = cv2.line(mask, (a,b),(c,d), color[i].tolist(), 2) #현재와 이전의 프레임을 이어줌
+                realframe = cv2.circle(realframe,(a,b),5,color[i].tolist(),-1)
+
+            # Now update the previous frame and previous points
+            old_gray = frame_gray.copy()
+            p0 = good_new.reshape(-1,1,2)
+            print("all ")
 
 
         ###Background UI 삽입
-        backgroundUI.setImage(realframe,0,0)
+        backgroundUI.setImage(realframe, 0, 0)
+       # wearing.setImage(realframe, 0, 0)
 
-        wearing.setImage(realframe,0,0)
+        if(st.all() == 1):
+            img = cv2.add(realframe, mask)
+        else:
+            print("else")
+            img = realframe
 
-        img = cv2.add(realframe,mask)
-        cv2.imshow('frame',img)
+        cv2.imshow('frame', img)
 
-
-        #카메라 종료
+        # 카메라 종료
         k = cv2.waitKey(30) & 0xff
-        if k == 27: #esc key
+        if k == 27:  # esc key
             cap.release()
             break
-
-        # Now update the previous frame and previous points
-        old_gray = frame_gray.copy()
-        p0 = good_new.reshape(-1,1,2)
 
     cap.release()
     cv2.destroyAllWindows()
